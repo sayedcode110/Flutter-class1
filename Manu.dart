c3f1ad5f15fb1fe9f643b03998a660aae7e69274
SayedCode <salehbusiness110@gmail.com>
2026-09-19 14:27:55 +0500

    Initial commit

---
+++ Manu.dart
@@ -0,0 +1,27 @@
+import 'dart.io';
+
+void main(){
+   print ("Cheeze Bager");
+   print ("Cheeze Pizza");
+   print ("Masala Frise");
+   print ("Biryani");
+   print ("Select For 1 Dish")        
+
+   String input = stdin.readLineSync()!;
+
+   switch(){
+    case 1:
+    print ("Cheeze Bager");
+    case 2:
+    print ("Cheeze Pizza");
+    case 3:
+    print ("Masala Frise");
+    case 4:
+    print ("Biryani");
+    case 5:
+    
+
+
+   }
+
+}
