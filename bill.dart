8f3805a2d79edbf5e56c377cf129bb0a3709c7d8
SayedCode <salehbusiness110@gmail.com>
2026-09-19 14:44:16 +0500

    Added bill calculator

---
+++ bill.dart
@@ -0,0 +1,51 @@
+void main() {
+  String customerName = "Syed Saleh";
+  int age = 18;
+  bool isMember = true;
+
+  // Home items
+  double ricePrice = 300;
+  int riceQuantity = 1;
+
+  double oilPrice = 500;
+  int oilQuantity = 2;
+
+  double sugarPrice = 200;
+  int sugarQuantity = 5;
+
+  // Calculate original bill
+  double totalBill =
+      (ricePrice * riceQuantity) +
+      (oilPrice * oilQuantity) +
+      (sugarPrice * sugarQuantity);
+
+  double discount = 0;
+
+  // Apply discount
+  if (totalBill >= 10000) {
+    discount = 20;
+  } else if (totalBill >= 5000) {
+    discount = 10;
+  } else {
+    discount = 0;
+  }
+
+  double discountAmount = totalBill * discount / 100;
+
+  double finalBill = totalBill - discountAmount;
+
+  // Additional member discount
+  if (isMember && finalBill > 5000) {
+    double memberDiscount = finalBill * 5 / 100;
+
+    finalBill = finalBill - memberDiscount;
+    discountAmount = discountAmount + memberDiscount;
+  }
+
+  // Print bill
+  print("Customer Name: $customerName");
+  print("Age: $age");
+  print("Original Bill: Rs. $totalBill");
+  print("Discount: Rs. $discountAmount");
+  print("Final Bill: Rs. $finalBill");
+}
